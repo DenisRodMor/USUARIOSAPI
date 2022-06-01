b7b07a19f846fc865ccd2a5dee8333cf9c490058
DenisRodMor <104526631+DenisRodMor@users.noreply.github.com>
2022-06-01 16:47:44 -0600

    Agregar archivos de proyecto.

---
+++ Usuarios.cs
@@ -0,0 +1,29 @@
+﻿namespace USUARIOSAPI
+{
+    public class Usuarios
+    {
+        public int Id { get; set; }
+
+        
+        public string Nombre { get; set; } = string.Empty;
+
+       
+        public string Apellidos { get; set; } = string.Empty;
+
+       
+        public string Usuario { get; set; } = string.Empty;
+
+       
+        public string Clave { get; set; } = string.Empty;
+
+        
+        public string Direccion { get; set; } = string.Empty;
+
+       
+        public string Telefono { get; set; } = string.Empty;
+
+        
+        public string TipodeUsuario { get; set; } = string.Empty;
+
+    }
+}
