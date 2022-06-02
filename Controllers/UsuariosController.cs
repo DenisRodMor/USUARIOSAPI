d9556980d4373683e50f2ef28f0bacf9cb8c59c7
DenisRodMor <104526631+DenisRodMor@users.noreply.github.com>
2022-06-01 18:16:12 -0600

    Terminando constructor

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace USUARIOSAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+       private static List<Usuarios> usuarios = new List<Usuarios>
+            {
+                new Usuarios{
+                    Id = 1,
+                    Nombre="Dennis",
+                    Apellidos = "Rodriguez Mora",
+                    Usuario = "Dejeromo",
+                    Clave="123456",
+                    Direccion="Alto Murillo",
+                    Telefono="83655072",
+                    TipodeUsuario="Administrador"
+                }
+            };
+
+        [HttpGet]
+        public async Task<ActionResult<List<Usuarios>>> Get()
+        {
+            
+            return Ok(usuarios);
+        }
+
+      
+
+        [HttpPost]
+        public async Task<ActionResult<List<Usuarios>>> AddUsuario(Usuarios user)
+        {
+            usuarios.Add(user);
+            return Ok(usuarios);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Usuarios>>> UpdateUsuario(Usuarios request )
+        {
+            var user = usuarios.Find(u => u.Id == request.Id);
+            if (user == null)
+                return BadRequest("Usuario no encontrado");    
+           
+            user.Nombre = request.Nombre;
+            user.Apellidos = request.Apellidos;
+            user.Usuario = request.Usuario;
+            user.Clave = request.Clave;
+            user.Direccion = request.Direccion;
+            user.Telefono = request.Telefono;
+            user.TipodeUsuario = request.TipodeUsuario;
+            return Ok(usuarios);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Usuarios>>> Delete(int id)
+        {
+            var user = usuarios.Find(u => u.Id == id);
+            if (user == null)
+                return BadRequest("Usuario no encontrado");
+            usuarios.Remove(user);
+            return Ok(usuarios);
+        }
+         
+
+    }
+}
